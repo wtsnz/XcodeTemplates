caad333691d48da9a8b2a7c3252dad244e0c01c7
Walig Castain <c.walig@gmail.com>
2015-07-02 15:52:44 +1200

    Added UITableViewController Swift template

---
+++ Developer/Xcode/Templates/File Templates/Custom Swift/TableViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,62 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___ : UITableViewController {
+
+    // MARK: - Properties
+
+    
+    // MARK: - Initializers
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+//        self.tableView.registerClass(MyCell.self, forCellReuseIdentifier: "MyCellIdentifier")
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - View
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: Methods
+    
+    
+    // MARK: - UITableViewDelegate, UITableViewDataSource
+    
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        
+//    }
+
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        
+//    }
+
+//    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        var cell = tableView.dequeueReusableCellWithIdentifier("MyCellIdentifier") as! MyCell
+//        return cell;
+//    }
+    
+//    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+//        
+//    }
+
+
+}
