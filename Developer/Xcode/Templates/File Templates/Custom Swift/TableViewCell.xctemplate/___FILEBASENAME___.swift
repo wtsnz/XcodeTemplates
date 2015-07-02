154a2fe85bc90484a8c8f68a4168f83d39e2b65b
Walig Castain <c.walig@gmail.com>
2015-07-02 15:57:55 +1200

    Updated Swift UITableViewCell template

--- Developer/Xcode/Templates/File Templates/Custom Swift/TableViewCell.xctemplate/___FILEBASENAME___.swift
+++ Developer/Xcode/Templates/File Templates/Custom Swift/TableViewCell.xctemplate/___FILEBASENAME___.swift
@@ -9,40 +9,44 @@
 import Foundation
 import UIKit
 
+let ___FILEBASENAMEASIDENTIFIER___ReuseIdentifier = "___FILEBASENAMEASIDENTIFIER___"
 
 class ___FILEBASENAMEASIDENTIFIER___ : UITableViewCell {
-
-    // MARK: - Properties
+    
+    // MARK: - Variables    
 
     
     // MARK: - Initializers
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.commonInit()
+
+        //init
+
+        self.setupAutoLayoutConstraints()
     }
     
     required init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
     // MARK: - Methods
-    
-    private func commonInit() {
-        self.setupAutoLayoutContraints()
-    }
-    
-    func configureWith() {
-        
-    }
-    
-    override func prepareForReuse() {
 
-    }
-    
     private func setupAutoLayoutContraints() {
-        
+
+    }
+    
+    func configureWith(name: String, selected: Bool) {
 
     }
 
+    // MARK: - Override
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+    }
+
+    
 }
